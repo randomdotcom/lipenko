f5a01e23bbd2e3d7d6275abbfa59acc95b83da00
truckit-maksim <maksim.lipenka@softensity.com>
2020-06-11 22:09:17 +0300

    fix host url

--- api/src/app.js
+++ api/src/app.js
@@ -8,7 +8,7 @@ var router = require("./routes");
 
 var app = express();
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "*");
@@ -22,12 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, "public")));
 app.use("/public", express.static(path.resolve(__dirname, "../public")));
 
-app.use(express.static(path.join(__dirname, '../build')))
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'))
-})
-
 initializeDb(() => {
   app.use(passport.initialize());
   passport.jwtStrategy();
@@ -35,13 +29,19 @@ initializeDb(() => {
 
   app.use("/api/", router);
 
-  app.get("*", function(req, res, next) {
+  app.use(express.static(path.join(__dirname, "../build")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build", "index.html"));
+  });
+
+  app.get("*", function (req, res, next) {
     let err = new Error("Page Not Found");
     err.statusCode = 404;
     next(err);
   });
 
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     console.error(err.message);
     if (!err.statusCode) err.statusCode = 500;
     res.status(err.statusCode).send(err.message);
