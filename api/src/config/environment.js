c7236353f26d3fba64f10d1d8c399abadb9a532a
truckit-maksim <maksim.lipenka@softensity.com>
2020-06-11 20:01:51 +0300

    production environment

--- api/src/config/environment.js
+++ api/src/config/environment.js
@@ -1,5 +1,6 @@
 const dotenv = require("dotenv");
 const joi = require("joi");
+dotenv.config();
 
 const schema = joi
   .object({
@@ -12,12 +13,10 @@ const schema = joi
       .string()
       .default("e5a3388c-9731-4043-8b11-be602d8c8919"),
     JWT_EXPIRATION: joi.number().default(21600000),
-    MONGODB_HOST: joi.string().default("mongodb://127.0.0.1:27017/nodejs-example")
+    MONGODB_HOST: joi.string().default(process.env.MONGODB_HOST)
   })
   .unknown()
   .required();
-
-dotenv.config();
 
 const { error, value: envVars } = joi.validate(process.env, schema);
 
