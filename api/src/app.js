57a70bdb6f0ca95f5d9d18a429790da86dbf47d6
truckit-maksim <maksim.lipenka@softensity.com>
2020-06-12 10:07:33 +0300

    fix host url

--- api/src/app.js
+++ api/src/app.js
@@ -27,9 +27,9 @@ initializeDb(() => {
   passport.jwtStrategy();
   passport.googleStrategy();
 
-  app.use("/api/", router);
-
   app.use(express.static(path.join(__dirname, "../build")));
+
+  app.use("/api/", router);
 
   app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../build", "index.html"));
